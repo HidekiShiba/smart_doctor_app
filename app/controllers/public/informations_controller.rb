da8888525334f3de26e59de1377a1e869c1f3a9e
HidekiShiba <hshiba033@gmail.com>
2021-01-24 03:00:06 +0000

    make_favorites_not_asynchronous

--- app/controllers/public/informations_controller.rb
+++ app/controllers/public/informations_controller.rb
@@ -1,7 +1,9 @@
 class Public::InformationsController < ApplicationController
   def index
+    @informations = Information.page(params[:page])
   end
 
   def show
+    @information = Information.find(params[:id])
   end
 end
