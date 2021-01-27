464b62792f4ce958975dfcd4093b281a704332c7
HidekiShiba <hshiba033@gmail.com>
2021-01-27 10:16:06 +0000

    setup_integer_columns_default

--- db/migrate/20210119023402_create_reservations.rb
+++ db/migrate/20210119023402_create_reservations.rb
@@ -3,17 +3,17 @@ class CreateReservations < ActiveRecord::Migration[5.2]
     create_table :reservations do |t|
       t.integer :patient_id,         null: false
       t.datetime :date_time,         null: false
-      t.integer :examination_count,  null: false, default: ""
+      t.integer :examination_count,  null: false
       t.string :symptom,             null: false, default: ""
       t.string :body_parts,          null: false, default: ""
       t.date :starting_point,        null: false
-      t.integer :treatment,          null: false, default: ""
+      t.integer :treatment,          null: false
       t.string :prescription,                     default: ""
-      t.integer :side_effect,        null: false, default: ""
+      t.integer :side_effect,        null: false
       t.string :other_diseases,                   default: ""
       t.string :hospital_name,                    default: ""
-      t.integer :pregnancy,          null: false, default: ""
-      t.integer :purpose,            null: false, default: ""
+      t.integer :pregnancy,          null: false
+      t.integer :purpose,            null: false
       t.string :request,                          default: ""
       t.integer :examination_status, null: false, default: 0
       
