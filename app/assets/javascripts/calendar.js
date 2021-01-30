9bb8c73bda395fb0bc60231a36ed6e16817015ba
HidekiShiba <hshiba033@gmail.com>
2021-01-30 15:06:23 +0000

    display_fullcalendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -6,13 +6,20 @@ $(function () {
     header: {
             // left: 'month, agendaWeek, agendaDay',
             left: 'title',
-            right: 'today prev,next'
+            right: 'today, prev, next'
     },
     views: {
             month: {columnFormat: 'ddd',},
             week: {columnFormat: 'M/D[(]ddd[)]',},
     },
-    // timeZone: 'locale',
+    
+    validRange: function(nowDate) {
+      return {
+        start: nowDate,
+        end: nowDate.clone().add(1, 'months')
+      };
+    },
+    // timeZone: 'Asia/Tokyo',
     // locale: 'ja',
     defaultView: 'agendaWeek',
     // navLinks: true,
@@ -28,7 +35,7 @@ $(function () {
     selectHelper: true,
     
     // 診療時間の表示→月曜しか反映されない。
-    businessHours: true,
+    // businessHours: true,
     businessHours:[{            
       dow: [ 1, 2, 3, 4, 5, 6 ], 
       start: '09:00',
@@ -40,20 +47,16 @@ $(function () {
       end: '19:00', 
     }],
     // 診療時間の表示→月曜しか反映されない。
+    selectConstraint: "businessHours",
+
+    events: "/reservations.json",
+    eventColor: '#EEFFFF',
+    eventTextColor: '#000000',
+    // displayEventTime: false,
+    overlap: false,
     
-    // DBデータの表示→表示されない。
-    reservations: '/reservations.json',
-    reservations:[{
-    id: 'json.id',
-    title: '×',
-    start: 'json.start_time',
-    end: 'json.end_time',
-    }],
-    color: 'yellow',
-    textColor: 'black',
-    // overlap: false,
-    // weekends: true,
-    // DBデータの表示→表示されない。
+    // nowIndicator: true,
+    // dayMaxEvents: true,
     
     select: function(startDate, endDate) {
       // alert('selected ' + startDate.format() + ' to ' + endDate.format());
